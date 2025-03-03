ecdcc096107be9b5c9f2c1cc10ec04223c9e0521
George <wallzy@mail.ru>
2025-03-03 13:48:52 +0300

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,37 @@
 import { Link } from "react-router";
-import { motion } from "motion/react";
-import { useState, useEffect, useRef, useCallback } from "react";
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 
 function Header({ headerHeight, setHeight }) {
-  const [isVisible, setVisible] = useState(true); // Начально элемент виден
-  const [lastScrollY, setLastScrollY] = useState(0); // Отслеживаем положение прокрутки
-  const header = useRef();
+  const headerRef = useRef();
 
-  const handleScroll = useCallback(() => {
-    if (window.scrollY > lastScrollY) {
-      // Если прокрутка идет вниз
-      setVisible(false); // Скрываем элемент
-    } else {
-      // Если прокрутка идет вверх
-      setVisible(true); // Показываем элемент
+  // Устанавливаем высоту хэдера
+  useEffect(() => {
+    if (headerRef.current) {
+      setHeight(headerRef.current.offsetHeight);
     }
-
-    setLastScrollY(window.scrollY); // Обновляем позицию прокрутки
-  }, [lastScrollY]);
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll); // Добавляем обработчик прокрутки
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll); // Убираем обработчик при размонтировании
-    };
-  }, [handleScroll]); 
-
-  useEffect(() => {
-    setHeight(header.current.offsetHeight);
   }, [setHeight]);
 
   return (
     <motion.div
-      ref={header}
+      ref={headerRef}
       className="lcontainer"
+      // animate={{ y: 0 }} // Используем y вместо top
+      // transition={{ duration: 0.3, ease: 'easeInOut' }} // Плавность
       style={{
-        position: 'fixed',
-        left: '0',
-        top: '0',
+        position: 'absolute', // Фиксируем относительно контейнера
+        left: 0,
+        top: 0, // Начальная позиция
         width: '100%',
         zIndex: 5,
       }}
-      animate={{ top: isVisible ? '0' : `-${headerHeight}px` }} // Анимация: скрытие/появление
-      transition={{ duration: 0.3 }} // Длительность анимации
     >
       <header>
         <a href="/" className="header__logo">SHIGAPOV</a>
         <nav className="header__navigation">
           <Link to="/">Главная</Link>
           <Link to="/contacts">Контакты</Link>
-          {/* Якорная ссылка на раздел "Цены" на главной странице */}
           <a href="/#prices">Цены</a>
         </nav>
         <a className="header__phone" href="tel:+89118428490">8 911 842 84 90</a>
